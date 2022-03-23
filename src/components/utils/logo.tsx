d88681b3a996a13f0faab693f1ae4ba556d344df
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2022-03-23 19:58:34 -0300

    bla

--- src/components/utils/logo.tsx
+++ src/components/utils/logo.tsx
@@ -1,12 +1,5 @@
 import { HStack, VStack, Heading, Text } from "@chakra-ui/react";
 import { RiHomeSmileLine } from "react-icons/ri";
-<<<<<<< HEAD
-import IconBox from "./iconBox";
-
-export const Logo = () => {
-  return (
-    <HStack alignItems="flex-end" pb="5" w="full">
-=======
 import { useRouter } from "next/router";
 import IconBox from "./iconBox";
 
@@ -17,23 +10,10 @@ export const Logo = () => {
   }
   return (
     <HStack alignItems="flex-end" pb="5" w="full" cursor="pointer" onClick={handleLogo}>
->>>>>>> main
       <IconBox color="white" bg="teal.300" p="2">
         <RiHomeSmileLine size="30" />
       </IconBox>
       <VStack alignItems="flex-start">
-<<<<<<< HEAD
-        <Heading m="0" p="0" fontSize="md" fontWeight="800" color="gray" flexWrap="nowrap" whiteSpace="nowrap">
-          Secretaria Virtual
-        </Heading>
-        <Text mt="-4px !important" p="0" fontSize="0.9em" color="gray.400">Flor de Jagube</Text>
-=======
-        <Heading
-          m="0"
-          p="0"
-          fontSize="md"
-          fontWeight="800"
-          color="gray.600"
           flexWrap="nowrap"
           whiteSpace="nowrap"
         >
@@ -42,7 +22,6 @@ export const Logo = () => {
         <Text mt="-4px !important" p="0" fontSize="0.9em" color="gray.500">
           Flor de Jagube
         </Text>
->>>>>>> main
       </VStack>
     </HStack>
   );
